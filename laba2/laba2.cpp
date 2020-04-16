5e4a3438116928a4fe81abe5d35b9ad0385b0e84
denis.timofeev <elena.timofeeva.0051@gmail.com>
2020-04-16 17:07:16 +0300

    Добавьте файлы проекта.

---
+++ laba2/laba2.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int f1(int* m, int n)
+{
+	int p = n - 1;
+	for (int i = p; i >= 0; i--)
+		cout << m[i] << " ";
+
+	return 0;
+}
+
+
+int main()
+{
+	int n = 0;
+	cout << "N = ";
+	cin >> n;
+	int* m = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> m[i];
+	}
+	f1(m, n);
+	cout << endl;
+
+	system("pause");
+}
