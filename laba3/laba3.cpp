7209ebbc6f8d31bab87cbe93e2b09c30e3642427
denis.timofeev <elena.timofeeva.0051@gmail.com>
2020-04-16 17:09:47 +0300

    Добавьте файлы проекта.

---
+++ laba3/laba3.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+using namespace std;
+
+struct element {
+	int value;
+	element* pointer;
+	element() {
+		value = 0;
+		pointer = NULL;
+	}
+};
+
+void In(element*& s, int n)
+{
+	element* p = NULL;
+	int i = 0;
+	cout << "Elementi: ";
+	while (i < n) {
+		element* p1 = new element();
+		cin >> p1->value;
+		if (s == NULL) {
+			s = p1;
+			p = p1;
+		}
+		else {
+			p->pointer = p1;
+			p = p1;
+		}
+		i++;
+	}
+}
+
+void Out(element* l) {
+	cout << "Spisok: ";
+	while (l != NULL) {
+		cout << l->value << " ";
+		l = l->pointer;
+	}
+	cout << endl;
+}
+
+
+
+void Add(element*& head)
+{
+	element* new_head = 0;
+	for (element* pos = head; pos;)
+	{
+		element* next = pos->pointer;
+		pos->pointer = new_head;
+		new_head = pos;
+		pos = next;
+	}
+	head = new_head;
+}
+
+int main()
+{
+	element* first = NULL;
+	int n;
+	cout << "Razmer spiska: ";
+	cin >> n;
+	In(first, n);
+	Out(first);
+	Add(first);
+	Out(first);
+	system("pause");
+
+	return 0;
+}
